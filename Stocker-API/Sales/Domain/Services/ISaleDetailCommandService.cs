ebb66119632a0c4d1a012d8fffc50ca8b76a5252
RodrLH <u202212338@upc.edu.pe>
2024-06-07 20:59:11 -0500

    Delete 2nd Backend version
    Deleting 2nd Backend version files

--- Stocker-API/Sales/Domain/Services/ISaleDetailCommandService.cs
+++
@@ -1,9 +0,0 @@
-using Stocker_API.Sales.Domain.Model.Aggregates;
-using Stocker_API.Sales.Domain.Model.Commands;
-
-namespace Stocker_API.Sales.Domain.Services;
-
-public interface ISaleDetailCommandService
-{
-    Task<SaleDetail?> Handle(CreateSaleDetailCommand command);
-}
